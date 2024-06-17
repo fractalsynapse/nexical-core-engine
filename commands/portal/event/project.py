04f915570eca6549eaab07e24f530ca1b826b45b
Adrian Webb <adrian@webb.sh>
2024-06-16 23:12:44 -0400

    Adding creation and linking of summary and note document collections on project updates in the project portal event command.

--- commands/portal/event/project.py
+++ commands/portal/event/project.py
@@ -1,4 +1,5 @@
 from systems.commands.index import Command
+from utility.data import get_identifier
 
 
 class Project(Command('portal.event.project')):
@@ -9,19 +10,42 @@ class Project(Command('portal.event.project')):
 
     def _update_project(self, event):
         if event.operation == 'delete':
-            self._project.filter(
-                portal_name = self.portal,
+            self._team_document_collection.filter(
+                team__portal_name = self.portal,
+                team__external_id = event.team_id,
+                name__in = [
+                    "note-{}".format(event.id),
+                    "summary-{}".format(event.id)
+                ]
+            ).delete()
+
+            self._team_project.filter(
+                team__portal_name = self.portal,
+                team__external_id = event.team_id,
                 external_id = event.id
             ).delete()
 
             self.send('agent:projects:delete', event.export())
             self.success("Successfully deleted project: {}".format(event.id))
         else:
+            documents = event.documents if event.documents else []
+
             team = self.save_instance(self._team, None, {
                 'portal_name': self.portal,
                 'external_id': event.team_id,
                 'name': event.team_name
             })
+            note_collection_name = "note-{}".format(event.id)
+            note_collection = self.save_instance(self._team_document_collection, get_identifier(note_collection_name), {
+                'team': team,
+                'name': note_collection_name
+            })
+            summary_collection_name = "summary-{}".format(event.id)
+            summary_collection = self.save_instance(self._team_document_collection, get_identifier(summary_collection_name), {
+                'team': team,
+                'name': summary_collection_name
+            })
+
             self.save_instance(self._team_project, event.id, {
                 'team': team,
                 'name': event.name,
@@ -31,7 +55,10 @@ class Project(Command('portal.event.project')):
                 'temperature': event.temperature,
                 'top_p': event.top_p,
                 'repetition_penalty': event.repetition_penalty,
-                'team_document_collections': event.documents
+                'team_document_collections': documents + [
+                    note_collection.external_id,
+                    summary_collection.external_id
+                ]
             }, relation_key = True)
 
             self.send('agent:projects:update', event.export())
