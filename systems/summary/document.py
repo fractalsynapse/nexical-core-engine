a76f215252fb332ae33e20bc0bdfb060eed76c46
Adrian Webb <adrian@webb.sh>
2024-06-16 23:03:16 -0400

    Updating field values in the document system summary class.

--- systems/summary/document.py
+++ systems/summary/document.py
@@ -11,9 +11,8 @@ class DocumentSummarizer(BaseModelSummarizer):
             document_facade = 'team_document',
             provider = provider
         )
-
+        self.document_id_field = 'team_document_collection_id'
         self.document_group_field = 'name'
 
         self.embedding_collection = 'team_document'
-        self.embedding_id_field = 'collection_id'
-        self.embedding_group_field = 'document_id'
+        self.embedding_id_field = 'document_id'
