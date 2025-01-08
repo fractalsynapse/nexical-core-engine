2290576d7bc5049640ca97e42e0f2483bbc49e80
Adrian Webb <adrian@webb.sh>
2025-01-08 00:07:33 -0500

    Minor formatting updates in the summary portal event command processor.

--- commands/portal/event/summary.py
+++ commands/portal/event/summary.py
@@ -1,18 +1,21 @@
 from systems.commands.index import Command
 
 
-class Summary(Command('portal.event.summary')):
+class Summary(Command("portal.event.summary")):
 
     def exec(self):
-        self.data("Processing summary {} from {}".format(self.event_id, self.portal), self.event_fields)
+        self.data(
+            "Processing summary {} from {}".format(self.event_id, self.portal),
+            self.event_fields,
+        )
         self.event_wrapper(self._perform_summary)
 
     def _perform_summary(self, event):
-        if event.operation == 'delete':
+        if event.operation == "delete":
             self.delete_summary(event, self.portal)
-            self.send('agent:summarizer:delete', event.export())
+            self.send("agent:summarizer:delete", event.export())
             self.success("Successfully deleted summary: {}".format(event.id))
         else:
             summary = self.publish_summary(event, self.portal)
-            self.send('agent:summarizer:summary', summary.export())
-            self.success('Completed summary processing request successfully')
+            self.send("agent:summarizer:summary", summary.export())
+            self.success("Completed summary processing request successfully")
